4952334ab46121d2cf9a4871da708bcbac232898
college-website44 <mp0820041@gmail.com>
2026-04-15 08:59:26 -0700

    scriptfive.sh

---
+++ scriptfive.sh
@@ -0,0 +1,13 @@
+5.WAS TO LIST OF USERS LOGGED IN BY DATE 
+#!/bin/bash
+DATE=$(date)
+echo "user logged $DATE"
+
+#!/bin/bash 
+echo "========================================================================"
+echo " LIST OR USERS LOGGED USERS-TERMINAL-LOCATION-DAY-MONTH-DATE-TIME-ACTIVE"
+echo "========================================================================"
+last | head -10 
+echo "******************************************************************"
+echo "**                        LIST IS A COMPLETE                  **"
+echo "******************************************************************"
