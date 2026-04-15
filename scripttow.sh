d10d10e43c910abdcd816dfe4da4121c077b2cad
college-website44 <mp0820041@gmail.com>
2026-04-15 08:55:45 -0700

    scripttow.sh

---
+++ scripttow.sh
@@ -0,0 +1,10 @@
+2.WAS CHECKING REMOTE SERVER CONNECTIVITY 
+
+#!/bin/bash
+echo "==========================="
+echo " CHECK SERVER CONNECTIVITY "
+echo "==========================="
+ping google.com | head -10
+echo "********************************************"
+echo "**   CHECKING COMPLETE                    **"
+echo "********************************************"
